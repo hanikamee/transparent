5ac2a475d4d8291039b0e48eb7438a0cf5cd3125
hanikamee <96562846+hanikamee@users.noreply.github.com>
2022-05-20 18:53:02 -0500

    Add files via upload

---
+++ Hackathon2022.py
@@ -0,0 +1,16 @@
+# TRANSPARENT - STUDENT FINANCE
+def main():
+    # mission statement
+    print("Welcome to Transparent! \n")
+    print("We believe that school should not get in the way of a student's financial future.")
+    print("Therefore, we created Transparent to clearly communicate to students their best path(s) to future financial success! \n")
+    def savings():
+        prompt = input("Do you have any scholarships, FAFSA money, or savings from a 529 or other account? (reply either 'yes' or 'no'). \n")
+        if prompt == 'yes':
+            print("Below, if the prompt does not apply to you, enter '0'. \n")
+            s_529 = input("Enter your 529 savings amount below: \n")
+            s_sav = input("Enter the total of any other savings you may have: \n")
+            s_
+    
+
+main()
