d4efb477c26dcbab5be72ca93c8d36f993b507ae
realrobhall <105953525+realrobhall@users.noreply.github.com>
2022-05-20 19:09:30 -0500

    Update Hackathon2022.py

--- Hackathon2022.py
+++ Hackathon2022.py
@@ -3,20 +3,30 @@ def main():
     # mission statement
     print("Welcome to Transparent! \n")
     print("We believe that school should not get in the way of a student's financial future.")
-    print("Therefore, we created Transparent to clearly communicate to students their best path(s) to future financial success! \n")
+    print("Therefore, we created Transparent to clearly communicate to students their best path(s) to future financial success! \n")a
+    savings_total = 0
     def savings():
         prompt = input("Do you have any scholarships, FAFSA money, or savings from a 529 or other account? (reply either 'yes' or 'no'). \n")
         if prompt == 'yes':
-            print("Below, if the prompt does not apply to you, enter '0'. \n")
+            print("Below, if the prompt does not apply to you, enter '0'. Do not use commas or any special characters. \n")
             s_529 = input("Enter your 529 savings amount below: \n")
             s_sav = input("Enter the total of any other savings you may have: \n")
-            s_
+            s_fafsa = input("Enter the total amount of FAFSA money you will be receiving. \n")
+            s_sch = input("Enter the total amount of scholarship money you will be receiving for the given school. \n")
+            savings_total = s_529 + s_sav + s_fafsa + s_sch
+            return savings_total
+        elif prompt == 'no':
+            continue
+        else:
+            print("Invalid Statement. ")
+            savings()
+            
     savings()
     def semester():
         # input savings amounts
     semester()
     def loan():
-        # input loan information
+        
     loan()
     def living():
         # input living expenses
